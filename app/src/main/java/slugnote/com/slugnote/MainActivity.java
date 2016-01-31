92c176a326e9ba24df7c43eaad9e53faf2e74d79
Ash Robbins <asrobbin@ucsc.edu>
2016-01-30 17:40:34 -0800

    courses and categories layout began, fragment beginning.

--- app/src/main/java/slugnote/com/slugnote/MainActivity.java
+++ app/src/main/java/slugnote/com/slugnote/MainActivity.java
@@ -1,53 +1,197 @@
 package slugnote.com.slugnote;
 
 import android.app.ActionBar;
+import android.app.Activity;
 import android.app.FragmentTransaction;
+import android.app.TabActivity;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TabHost;
 
-public class MainActivity extends FragmentActivity{
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import org.json.JSONArray;
+
+import java.util.ArrayList;
+import java.util.List;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity{
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+   // private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        Button categories = (Button)findViewById(R.id.button1);
+        Button search = (Button)findViewById(R.id.button2);
+        Button upload = (Button)findViewById(R.id.button3);
+        Button notes = (Button)findViewById(R.id.button4);
+        Button settings = (Button)findViewById(R.id.button5);
+
+
+        categories.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent1);
+            }
+        });
+
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(getApplicationContext(),searchFragment.class);
+                startActivity(intent1);
+            }
+        });
+
+        upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(getApplicationContext(),uploadFragment.class);
+                startActivity(intent1);
+            }
+        });
+
+        notes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(getApplicationContext(),notesFragment.class);
+                startActivity(intent1);
+            }
+        });
+
+        settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(getApplicationContext(),settingsFragment.class);
+                startActivity(intent1);
+            }
+        });
+
+        ListView list = (ListView) findViewById(R.id.listViewColleges);
+
+        try{
+            String in = "[\"UCSC\",\"UCSD\",\"UCLA\",\"UCSB\",\"UCSF\"]";
+            JSONArray jsIn = new JSONArray(in);
+            int len = jsIn.length();
+            List<String> lc = new ArrayList<>(len);
+            for(int i = 0; i< len ; i++){
+                lc.add(jsIn.getString(i));
+            }
+            list.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,lc));
+            list.setOnItemClickListener(new OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    String temp = ((TextView)view).getText() + Integer.toString(position);
+                    Toast.makeText(getApplicationContext(),temp ,Toast.LENGTH_SHORT ).show();
+                }
+            });
+        }
+        catch (Exception e){
+            //
+        }
+
+
+
         //create tabhost to have tabs
-        TabHost tabHost = (TabHost)findViewById(android.R.id.tabhost);
-
-
+//        TabHost tabHost = (TabHost) findViewById(R.id.tabhost);
+//        System.out.println("aosdjalksdjaskldh" + tabHost);
+//        tabHost.setup();
         //different tabs
-        TabHost.TabSpec categories = tabHost.newTabSpec("Categories");
-        TabHost.TabSpec search=tabHost.newTabSpec("Search");
-        TabHost.TabSpec upload=tabHost.newTabSpec("Upload");
-        TabHost.TabSpec notebook=tabHost.newTabSpec("Notebook");
+//        TabHost.TabSpec categories = tabHost.newTabSpec("Categories");
+//        TabHost.TabSpec search = tabHost.newTabSpec("Search");
+//        TabHost.TabSpec upload = tabHost.newTabSpec("Upload");
+//        TabHost.TabSpec notebook = tabHost.newTabSpec("Notebook");
 
-        categories.setIndicator("Cat");
-        categories.setContent(new Intent(this, categoriesFragment.class));
-
-        search.setIndicator("Sea");
-        search.setContent(new Intent(this, searchFragment.class));
-
-
-        upload.setIndicator("Upl");
-        //add upload and notebook intents here
-
-        notebook.setIndicator("Not");
+//        categories.setIndicator("", getResources().getDrawable(R.drawable.ic_launcher));
+//        categories.setIndicator("Poop");
+//        Intent categoriesIntent= new Intent(this, categoriesFragment.class);
+//        categories.setContent(categoriesIntent);
+//        categories.setContent(R.id.tab1);
+//        tabHost.addTab(categories);
+//        search.setIndicator("Sea");
+//        Intent searchIntent = new Intent(this, searchFragment.class);
+//        search.setContent(searchIntent);
+//
+//
+//        upload.setIndicator("Upl");
+//        Intent uploadIntent = new Intent(this, uploadFragment.class);
+//        upload.setContent(uploadIntent);
+//        //add upload and notebook intents here
+//
+//        notebook.setIndicator("Not");
+        //Intent notebookIntent = new Intent(this, notebookFragment.class);
+        //notebook.setContent(notebookIntent);
 
         //add actual tabs
-        tabHost.addTab(categories);
-        tabHost.addTab(search);
-        tabHost.addTab(upload);
-        tabHost.addTab(notebook);
+//        System.out.println("Categories:" + categories);
+//        tabHost.addTab(categories);
+//        tabHost.addTab(search);
+//        tabHost.addTab(upload);
+//        tabHost.addTab(notebook);
 
 
     }
 
 
+    @Override
+    public void onStart() {
+        super.onStart();
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+       // client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://slugnote.com.slugnote/http/host/path")
+        );
+       // AppIndex.AppIndexApi.start(client, viewAction);
+    }
 
+    @Override
+    public void onStop() {
+        super.onStop();
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://slugnote.com.slugnote/http/host/path")
+        );
+       // AppIndex.AppIndexApi.end(client, viewAction);
+        //client.disconnect();
+    }
 }
--- app/src/main/java/slugnote/com/slugnote/coursesFragment.java
+++ app/src/main/java/slugnote/com/slugnote/coursesFragment.java
@@ -1,44 +1,115 @@
 package slugnote.com.slugnote;
 
-import android.app.ListFragment;
+import android.app.ActionBar;
+import android.app.Activity;
+import android.app.FragmentTransaction;
+import android.app.TabActivity;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TabHost;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import org.json.JSONArray;
+
+import java.util.ArrayList;
+import java.util.List;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 /**
  * Created by Ash on 1/30/2016.
  */
-public class coursesFragment extends ListFragment{
 
-    //SEND ID To server, recieve list of courses here
-    //get id from
-
-
-    //hard coded array
-    String[] courses = {"CMPE8", "CMPS5J", "WRITING 2", "CE100"};
+public class coursesFragment extends Activity {
 
     @Override
-    public View onCreateView(LayoutInflater inf, ViewGroup container, Bundle savedInstanceState)
-    {
-        View rootView=inf.inflate(R.layout.courses, container,false);
-
-        //adapter here
-        ListAdapter adapt = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1);
-        setListAdapter(adapt);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.courses);
 
 
-        return rootView;
+        Button categories = (Button) findViewById(R.id.button1);
+        Button search = (Button) findViewById(R.id.button2);
+        Button upload = (Button) findViewById(R.id.button3);
+        Button notes = (Button) findViewById(R.id.button4);
+        Button settings = (Button) findViewById(R.id.button5);
+
+
+        categories.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent1);
+            }
+        });
+
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(getApplicationContext(), searchFragment.class);
+                startActivity(intent1);
+            }
+        });
+
+        upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(getApplicationContext(), uploadFragment.class);
+                startActivity(intent1);
+            }
+        });
+
+        notes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(getApplicationContext(), notesFragment.class);
+                startActivity(intent1);
+            }
+        });
+
+        settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(getApplicationContext(), settingsFragment.class);
+                startActivity(intent1);
+            }
+        });
+
+        ListView list = (ListView) findViewById(R.id.listViewCourses);
+
+        try {
+            String in = "[\"CMPE8\",\"CMPE100\",\"Writing 2\",\"CS5J\",\"CS101\"]";
+            JSONArray jsIn = new JSONArray(in);
+            int len = jsIn.length();
+            List<String> lc = new ArrayList<>(len);
+            for (int i = 0; i < len; i++) {
+                lc.add(jsIn.getString(i));
+            }
+            list.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, lc));
+            list.setOnItemClickListener(new OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    String temp = ((TextView) view).getText() + Integer.toString(position);
+                    Toast.makeText(getApplicationContext(), temp, Toast.LENGTH_SHORT).show();
+                }
+            });
+        } catch (Exception e) {
+            //
+        }
+
+
     }
-
-    @Override
-    public void onListItemClick(ListView l, View v, int position, long id){
-        //SEND ID to server, and call the courses fragment?
-
-        return;
-    }
-
 }
